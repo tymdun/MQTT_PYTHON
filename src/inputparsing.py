26e318d696b5d01b3f92c4a33c106f7964fea9c0
tymdun <tymdun@byu.edu>
2020-12-02 14:55:01 -0700

    Prints out the user info

--- src/inputparsing.py
+++ src/inputparsing.py
@@ -3,6 +3,18 @@ import sys
 import logging
 
 
+# This function decides wether to print to stdout or stderr depending on if help was called.
+def printhelp(helpNeeded):
+    message = """Usage: chat [--help] [-v] [-h HOST] [-p PORT] [-n NAME] NETID\n\nArguments:\n NETID The NetID of the user.\n\nOptions:\n --help\n -v, --verbose\n --host HOSTNAME, -h HOSTNAME\n --port PORT, -p PORT\n --name NAME, -n NAME"""
+    if helpNeeded:
+        print(message, file=sys.stdout)
+
+    else:
+        print(message, file=sys.stderr)
+
+    return 0
+
+
 def check_inputs():
     verbose = False
     help = False
@@ -15,10 +27,12 @@ def check_inputs():
     # print(argv)
     opt, args = getopt.getopt(
         argv, "h:p:n:v", ["host=", "port=", "name=", "verbose", "help"])
-    print(opt)
+    # print(opt)
 
     for opt, arg in opt:
-        if opt in ['-h', '--host']:
+        if opt in ['--help']:
+            printhelp(True)
+        elif opt in ['-h', '--host']:
             host = arg
         elif opt in ['-p']:
             port = arg
@@ -28,6 +42,8 @@ def check_inputs():
             verbose = True
 
     print()
+
+    return 0
 
 
 def mqtt_client_parse_arguments():
