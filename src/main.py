8b57cf1ce310f73d1410fab9c44307d81599220c
tymdun <tymdun@byu.edu>
2020-12-05 11:39:28 -0700

    Working on framing the gui

--- src/main.py
+++ src/main.py
@@ -2,17 +2,39 @@ import tkinter as tk
 import inputparsing
 import paho.mqtt.client as mqtt
 
+HEIGHT = 700
+WIDTH = 1000
+
 # print(inputparsing.mqtt_client_parse_arguments())
 configList = inputparsing.mqtt_check_inputs()
 print(configList)
-mqtt.Client(configList[2], clean_session=False, )
+client = mqtt.Client(configList[2], clean_session=False)
+#client.connect(configList[0], configList[1])
 
-exit()
+# exit()
 
 root = tk.Tk()
+root.title("MQTT CHAT ROOM")
 
-button = tk.Button(root, text="TEST BUTTON", bg='red')
-button.pack()
+canvas = tk.Canvas(root, height=HEIGHT, width=WIDTH)
+canvas.pack()
+
+
+onlineUsers = tk.Frame(root, bg='yellow')
+onlineUsers.place(relwidth=0.4, relheight=0.75)
+onlineText = tk.Text(onlineUsers)
+onlineText.pack(pady=0.1, padx=0.1, fill='both')
+onlineText.insert(tk.END, "HELLO WORLD")
+onlineText.config(state='disabled')
+
+messages = tk.Frame(root, bg='red')
+messages.place(relx=0.4, relwidth=0.6, relheight=0.75)
+
+inputBox = tk.Frame(root, bg='green')
+inputBox.place(rely=0.75, relwidth=1, relheight=0.25)
+
+button = tk.Button(inputBox, text="TEST BUTTON", bg='black')
+button.place(relheight=0.05, relwidth=0.1, rely=0.95, relx=0.5)
 
 
 root.mainloop()
