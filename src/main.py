926b11578c444fe8beb7a31198c1775cbf40b905
tymdun <tymdun@byu.edu>
2020-12-05 11:44:48 -0700

    Made the colors less ugly

--- src/main.py
+++ src/main.py
@@ -20,17 +20,17 @@ canvas = tk.Canvas(root, height=HEIGHT, width=WIDTH)
 canvas.pack()
 
 
-onlineUsers = tk.Frame(root, bg='yellow')
+onlineUsers = tk.Frame(root, bg='#018786')
 onlineUsers.place(relwidth=0.4, relheight=0.75)
 onlineText = tk.Text(onlineUsers)
 onlineText.pack(pady=0.1, padx=0.1, fill='both')
 onlineText.insert(tk.END, "HELLO WORLD")
 onlineText.config(state='disabled')
 
-messages = tk.Frame(root, bg='red')
+messages = tk.Frame(root, bg='#800080')
 messages.place(relx=0.4, relwidth=0.6, relheight=0.75)
 
-inputBox = tk.Frame(root, bg='green')
+inputBox = tk.Frame(root, bg='#192734')
 inputBox.place(rely=0.75, relwidth=1, relheight=0.25)
 
 button = tk.Button(inputBox, text="TEST BUTTON", bg='black')
